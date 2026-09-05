58abb872e63bd5611e5ad3df3308385492c68111
Matt W <mattw@Matts-MacBook-Pro-3.local>
2026-09-05 20:44:49 +0800

    fix: debounce battery widget refreshes

--- app/src/main/java/com/tommasoberlose/anotherwidget/global/Preferences.kt
+++ app/src/main/java/com/tommasoberlose/anotherwidget/global/Preferences.kt
@@ -131,6 +131,7 @@ object Preferences : KotprefModel() {
     var showBatteryCharging by booleanPref(default = false)
     var isBatteryLevelLow by booleanPref(default = false)
     var isCharging by booleanPref(default = false)
+    var lastBatteryLevel by intPref(default = -1)
     var googleFitSteps by longPref(default = -1)
     var showDailySteps by booleanPref(default = false)
     var showGreetings by booleanPref(default = false)
--- app/src/main/java/com/tommasoberlose/anotherwidget/receivers/BatteryLevelReceiver.kt
+++ app/src/main/java/com/tommasoberlose/anotherwidget/receivers/BatteryLevelReceiver.kt
@@ -3,11 +3,16 @@ package com.tommasoberlose.anotherwidget.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.tommasoberlose.anotherwidget.global.Preferences
 import com.tommasoberlose.anotherwidget.helpers.BatteryHelper
 import com.tommasoberlose.anotherwidget.ui.widgets.MainWidget
 
 class BatteryLevelReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        val wasCharging = Preferences.isCharging
+        val wasBatteryLevelLow = Preferences.isBatteryLevelLow
+        val previousBatteryLevel = Preferences.lastBatteryLevel
+
         when (intent.action) {
             Intent.ACTION_BATTERY_LOW,
             Intent.ACTION_BATTERY_OKAY,
@@ -20,7 +25,16 @@ class BatteryLevelReceiver : BroadcastReceiver() {
             }
             else -> return
         }
-        MainWidget.updateWidget(context)
+
+        val currentBatteryLevel = BatteryHelper.getBatteryLevel(context)
+        Preferences.lastBatteryLevel = currentBatteryLevel
+
+        if (wasCharging != Preferences.isCharging ||
+            wasBatteryLevelLow != Preferences.isBatteryLevelLow ||
+            previousBatteryLevel != currentBatteryLevel
+        ) {
+            MainWidget.updateWidget(context)
+        }
     }
 
 }
